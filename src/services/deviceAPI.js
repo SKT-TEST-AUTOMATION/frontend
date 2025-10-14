1730b60c89913ece72d0d3d158d7b291854e03ba
subbni <slfkak@naver.com>
2025-10-14 17:35:53 +0900

    [feat] Service Files 작성

---
+++ src/services/deviceAPI.js
@@ -0,0 +1,11 @@
+import { api } from "./axios";
+
+// 등록된 디바이스 목록 조회
+export async function getDevices(params = {}, signal) {
+  const { page = 0, size = 10, sort = "id,desc"} = params;
+  const res = await api.get("devices", {
+    signal,
+    params: { page, size, sort },
+  });
+  return res;
+}
